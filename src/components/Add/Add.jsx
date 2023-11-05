dc9da50711cdb321dff6af52b16e21f33935487e
fahim <basitabdullah1122@gmail.com>
2023-11-05 21:47:46 +0530

    Fire Storage integrated

--- src/components/Add/Add.jsx
+++ src/components/Add/Add.jsx
@@ -4,20 +4,66 @@ import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import noImg from "../../assets/noImage.jpg";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { auth, db, storage } from "../../firebase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { MdDriveFolderUpload } from "react-icons/md";
 const Add = (props) => {
   const [data, setData] = useState({});
   const [file, setFile] = useState("");
+  const [per, setPer] = useState(null);
 
+  useEffect(() => {
+    const uploadFile = () => {
+      const name = new Date().getTime() + file.name;
+      const storageRef = ref(storage, name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setPer(progress)
+            
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+          
+        },
+      
+        (error) => {
+          switch (error.code) {
+            case "storage/unauthorized":
+              break;
+            case "storage/canceled":
+              break;
+
+            case "storage/unknown":
+              break;
+          }
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+         setData((prev)=>({...prev, img:downloadURL}))
+         toast("Upload Successful")
+          });
+        }
+      );
+    };
+
+    file && uploadFile();
+  }, [file]);
   const handleInput = (e) => {
     const id = e.target.id;
     const value = e.target.value;
     setData({ ...data, [id]: value });
   };
 
-  console.log(data);
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -49,12 +95,17 @@ const Add = (props) => {
 
         <form onSubmit={handleSubmit}>
           <div className="imageContainer">
-            <img src={file ? URL.createObjectURL(file) : noImg } alt="" />
+            <img src={file ? URL.createObjectURL(file) : noImg} alt="" />
             <div className="fileContainer">
               <label htmlFor="file">
-               Upload Image : <MdDriveFolderUpload />
+                Upload Image : <MdDriveFolderUpload />
               </label>
-              <input  onChange={e=>setFile(e.target.files[0])} type="file" id="file" style={{display:"none"}} />
+              <input
+                onChange={(e) => setFile(e.target.files[0])}
+                type="file"
+                id="file"
+                style={{ display: "none" }}
+              />
             </div>
           </div>
 
@@ -71,7 +122,7 @@ const Add = (props) => {
                 />
               </div>
             ))}
-          <button type="submit">Send</button>
+          <button disabled={per != null && per < 100 }type="submit">Send</button>
         </form>
       </div>
     </div>
--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,6 +1,6 @@
 import "./users.scss";
 import { useState } from "react";
-import DataTableMui from "../../components/dataTableMui/DataTableMui"
+import DataTableMui from "../../components/dataTableMui/DataTableMui";
 import { userRows } from "../../data";
 import Add from "../../components/Add/Add";
 
@@ -58,9 +58,56 @@ const columns = [
   },
 ];
 
+const columnsTable = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    id: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 100,
+  },
+  {
+    field: "lastName",
+    id: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 100,
+  },
+  {
+    field: "email",
+    id: "email",
+    type: "string",
+    headerName: "Email",
+    width: 150,
+  },
+  {
+    field: "phone",
+    id: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 150,
+  },
+
+  {
+    field: "verified",
+    id: "verified",
+    headerName: "Verified",
+    width: 100,
+    type: "boolean",
+  },
+];
+
 const Users = () => {
   const [open, setOpen] = useState(false);
-
 
   return (
     <div className="users">
@@ -68,8 +115,8 @@ const Users = () => {
         <h1>Users</h1>
         <button onClick={() => setOpen(true)}>Add New User</button>
       </div>
-      <DataTableMui slug="users" columns={columns} rows={userRows} />
-    
+      <DataTableMui slug="users" columns={columnsTable} rows={userRows} />
+
       {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   );
